b3514c504f20b1484e1623541f21170d7a51a9d3
Erik Lima <erikhalima05@gmail.com>
2026-06-23 13:31:24 -0300

    feat: implement app/[locale]/layout.tsx to support i18n; test i18n implementation using example messages on page.tsx Client Component

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,52 @@
+import "@/app/globals.css";
+
+import { NextIntlClientProvider } from "next-intl";
+import {
+  getMessages,
+  getTranslations,
+  setRequestLocale,
+} from "next-intl/server";
+import { notFound } from "next/navigation";
+import { routing } from "@/i18n/routing";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
+
+  const t = await getTranslations({ locale, namespace: "LayoutMetadata" });
+
+  return {
+    title: t("title"),
+    description: t("description"),
+  };
+}
+
+export default async function LocaleLayout({
+  children,
+  params,
+}: Readonly<{
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
+}>) {
+  const { locale } = await params;
+
+  if (!routing.locales.includes(locale as (typeof routing.locales)[number]))
+    notFound();
+
+  setRequestLocale(locale);
+
+  const messages = await getMessages();
+
+  return (
+    <html lang="en" className={`h-full antialiased`}>
+      <body className="min-h-full flex flex-col">
+        <NextIntlClientProvider messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
