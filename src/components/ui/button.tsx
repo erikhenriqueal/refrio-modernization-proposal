69f6bf6265a20a1051afb61d83a93278a4622a49
Erik Lima <erikhalima05@gmail.com>
2026-06-25 13:04:48 -0300

    refactor: use new Button/Anchor components

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,16 +5,22 @@ const BASE =
 
 const VARIANTS = {
   primary: `${BASE}
-      transition-colors ease-in-out duration-200
-      bg-primary-500 border-primary-400 text-neutral-50
-      hover:bg-primary-600 hover:border-primary-500 hover:text-neutral-100
-      active:*:scale-95`,
+            transition-colors ease-in-out duration-200
+            bg-primary-500 border-primary-400 text-neutral-50
+            hover:bg-primary-600 hover:border-primary-500 hover:text-neutral-100
+            active:*:scale-95`,
   // Same as navbar language selector
   secondary: `${BASE}
-      transition-colors ease-in-out duration-100
-      bg-neutral-50 border-primary-100
-      hover:bg-primary-100 hover:border-primary-100
-      active:*:scale-95`,
+              transition-colors ease-in-out duration-100
+              bg-neutral-50 border-primary-100
+              hover:bg-primary-100 hover:border-primary-100
+              active:*:scale-95`,
+  // Same as navbar achors
+  underline: `relative p-2
+              transition-colors duration-200 ease-out
+              after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2] after:origin-left after:scale-x-0 after:bg-primary-600
+              after:transition after:duration-200 after:ease-out
+              hover:text-primary-600 hover:after:scale-x-100`,
 };
 
 const SHAPES = {
