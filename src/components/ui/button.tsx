8b9e179dd5a52a4d4b190e2489fe1e8feb006450
Erik Lima <erikhalima05@gmail.com>
2026-06-25 19:26:29 -0300

    refactor: reorganize variants declarations

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,26 +1,29 @@
 import React from "react";
 
 const BASE =
-  "p-2 border-2 shadow-md shadow-primary-900/35 cursor-pointer outline-none";
+  "inline-block p-2 border-2 shadow-md shadow-primary-900/35 cursor-pointer outline-none ";
 
 const VARIANTS = {
-  primary: `${BASE}
-            transition-colors ease-in-out duration-200
-            bg-primary-500 border-primary-400 text-neutral-50
-            hover:bg-primary-600 hover:border-primary-500 hover:text-neutral-100
-            active:*:scale-95`,
+  primary:
+    BASE +
+    "transition-colors ease-in-out duration-200 " +
+    "bg-primary-500 border-primary-400 text-neutral-50 " +
+    "hover:bg-primary-600 hover:border-primary-500 hover:text-neutral-100 " +
+    "active:*:scale-95",
   // Same as navbar language selector
-  secondary: `${BASE}
-              transition-colors ease-in-out duration-100
-              bg-neutral-50 border-primary-100
-              hover:bg-primary-100 hover:border-primary-100
-              active:*:scale-95`,
+  secondary:
+    BASE +
+    "transition-colors ease-in-out duration-100 " +
+    "bg-neutral-50 border-primary-100 " +
+    "hover:bg-primary-100 hover:border-primary-100 " +
+    "active:*:scale-95",
   // Same as navbar achors
-  underline: `relative p-2
-              transition-colors duration-200 ease-out
-              after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2] after:origin-left after:scale-x-0 after:bg-primary-600
-              after:transition after:duration-200 after:ease-out
-              hover:text-primary-600 hover:after:scale-x-100`,
+  underline:
+    "relative p-2 " +
+    "transition-colors duration-200 ease-out " +
+    "after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2] after:origin-left after:scale-x-0 after:bg-primary-600 " +
+    "after:transition after:duration-200 after:ease-out " +
+    "hover:text-primary-600 hover:after:scale-x-100 ",
 };
 
 const SHAPES = {
