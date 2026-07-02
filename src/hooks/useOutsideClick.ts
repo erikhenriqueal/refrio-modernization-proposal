7124053b255abdd2d4dcd9def11d679bdfe13e6c
Erik Lima <erikhalima05@gmail.com>
2026-07-02 09:53:55 -0300

    feat: create useOutsideClick hook

---
+++ src/hooks/useOutsideClick.ts
@@ -0,0 +1,24 @@
+"use client";
+
+import { useEffect } from "react";
+
+export function useOutsideClick(
+  refs: React.RefObject<HTMLElement>[],
+  callback: () => void,
+) {
+  useEffect(() => {
+    const handlePointerDown = (event: PointerEvent) => {
+      const target = event.target as Node;
+
+      const clickedInside = refs.some((ref) => ref.current?.contains(target));
+
+      if (!clickedInside) callback();
+    };
+
+    document.addEventListener("pointerdown", handlePointerDown);
+
+    return () => {
+      document.removeEventListener("pointerdown", handlePointerDown);
+    };
+  }, [refs, callback]);
+}
